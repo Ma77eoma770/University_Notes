34bddc51cc5d435bb07c4444da8539cbb60ae68e
Matt <matteologozzi@gmail.com>
2025-09-28 17:03:00 +0200

    Quartz sync: Sep 28, 2025, 5:03 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: null,
     locale: "en-US",
     baseUrl: "ma77eoma770.github.io/University_Notes/",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", ".obsidian", "media","references"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
